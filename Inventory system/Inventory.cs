3cf15c83357834d56a834247271255ae01c971f7
Brionna Franklin <s198017@students.aie.edu>
2019-08-23 10:06:11 -0500

    Add project files.

---
+++ Inventory system/Inventory.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_system
+{
+    class Inventory
+    {
+        int damage = 10;
+        float gold = 0.00f;
+        float minGold = 0.00f;
+        public void Menu()
+        {
+            string choice = "";
+            
+
+            while (choice != "0")
+            {
+                //Display
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("0: Exit");
+                Console.WriteLine("1: Equip Weapon");
+                Console.WriteLine("2: Unequip Weapon");
+                Console.WriteLine("3: Add Gold");
+                Console.WriteLine("4: Subtract Gold");
+
+                //get input
+                choice = Console.ReadLine();
+                Console.WriteLine("");
+
+                //check input
+                if (choice == "1")
+                {
+                    EquipWeapon();
+                }
+                else if (choice == "2")
+                {
+                    UnequipWeapon();
+
+                }
+                else if (choice == "3")
+                {
+                    Console.Write("How much gold? ");
+                    float addedGold = Convert.ToSingle(Console.ReadLine());
+                    AddGold(addedGold);
+                }
+                else if (choice == "4")
+                {
+                    Console.Write("How much gold? ");
+                    float subtractedGold = Convert.ToSingle(Console.ReadLine());
+                    SubtractGold(subtractedGold);
+                }
+            }
+        }
+        public void EquipWeapon()
+        {
+            string choice = "";
+            //choice weapon
+            Console.WriteLine("Equip a weapon! (1: Dagger / 2: Sword / 3: Bow / 4: Warhammer) press 0 to cancel.");
+            choice = Console.ReadLine();
+            Console.WriteLine("");
+
+            //check input
+            if (choice == "0")
+            {
+                Console.WriteLine("Action canceled");
+            }
+            else if (choice == "1")
+            {
+                damage = 15;
+                Console.WriteLine(" You've equided a dagger");
+                Console.WriteLine("Damage: " + damage);
+            }
+            else if (choice == "2")
+            {
+                    damage = 20;
+                Console.WriteLine(" You've equided a sword");
+                Console.WriteLine("Damage: " + damage);
+            }
+            else if (choice == "3")
+            {
+                damage = 25;
+                Console.WriteLine(" You've equided a bow");
+                Console.WriteLine("Damage: " + damage);
+            }
+            else if (choice == "4")
+            {
+                damage = 30;
+                Console.WriteLine(" You've equided a warhammer");
+                Console.WriteLine("Damage: " + damage);
+            }
+            else
+            {
+                EquipWeapon();
+            }
+        }
+        public void UnequipWeapon()
+        {
+            if (damage == 10)
+            {
+                Console.WriteLine("No weapon to unequiped.");
+                Console.WriteLine("Damage: " + damage);
+            }
+            else
+            {
+                Console.WriteLine("Unequiped a weapon!");
+                damage = 10;
+                Console.WriteLine("Damage: " + damage);
+            }
+        }
+
+        //money
+        public void AddGold(float amount)
+        {
+            Console.WriteLine("Got " + amount + " gold!");
+            gold += amount;
+            Console.WriteLine("Gold: " + gold);
+        }
+        public void SubtractGold(float amount)
+        {
+            Console.WriteLine("Lost " + amount + " gold!");
+            gold -= amount;
+            if (gold <= 0)
+            {
+                gold = minGold;
+            }
+            Console.WriteLine("Gold: " + gold);
+            
+        }
+    }
+}
---
+++ Inventory system/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_system
+{
+    class Program
+    {
+       static void Main(String[]args)
+        {
+            Inventory inventory = new Inventory();
+            inventory.Menu();
+
+        }
+    }
+}
